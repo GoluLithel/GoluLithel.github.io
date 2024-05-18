18916c04777de95d1b11a12eaeadad406ed60971
Bhogendra Kamble <bhogendra.kamble@spit.ac.in>
2024-05-18 13:31:26 +0530

    Add uuid to generate key

--- src/components/sections/contacts-page/ContactInfo.js
+++ src/components/sections/contacts-page/ContactInfo.js
@@ -4,7 +4,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import { contactDetails } from '../../../data/contactDetails';
-import generateRandomID from '../../../utils/generateRandomID';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function ContactInfo() {
 
@@ -21,13 +21,13 @@ export default function ContactInfo() {
                     {details.map((data, index) => {
                         if (index % 2 === 0) {
                             return (
-                                <TableRow key={generateRandomID()}>
+                                <TableRow key={uuidv4()}>
                                     <TableCell sx={{ display: { md: 'none', sm: 'none', xs: 'block' }, fontSize: '1.2rem', border: 'none', marginBottom: '-10px', paddingBottom: '0px' }}>{data[1]}&nbsp;&nbsp;&nbsp;<strong>{data[2]}</strong></TableCell>
                                 </TableRow>
                             );
                         } else {
                             return (
-                                <TableRow key={generateRandomID()} sx={{ borderBottom: 'solid #eeeeee 1px', paddingBottom: '50px' }}>
+                                <TableRow key={uuidv4()} sx={{ borderBottom: 'solid #eeeeee 1px', paddingBottom: '50px' }}>
                                     <TableCell sx={{ display: { md: 'block', sm: 'block', xs: 'none' }, fontSize: '1.2rem', border: 'none' }}>{data[1]}&nbsp;&nbsp;&nbsp;<strong>{data[2]}</strong></TableCell>
                                     <TableCell style={{ fontSize: '1.2rem', border: 'none' }}>
                                         <a href={data[3]}>
--- src/utils/generateRandomID.js
+++
@@ -1,3 +0,0 @@
-export default function generateRandomID() {
-  return 'id-' + Math.random().toString(36).slice(2, 16);
-}
